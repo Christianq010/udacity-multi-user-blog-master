e1ac9ae131c2c293632fd9594570d25b3ea9dd3c
Christiaan Quyn <christianq010@gmail.com>
2017-04-21 10:09:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Multi User Blog
+
+A simple multi user blog where users can sign in and post blog posts as well as 'Like' and 'Comment' on other posts made on the blog.
+
+## Usage
+Clone this repo to your desktop, go to its root directory (that contains the app.yaml) and run:
+```bash
+dev_appserver.py app.yaml
+```
+
+Or use the live app here:
+
+(https://multi-userblog1.appspot.com/)
+
+### Google App Engine Tips
+* `gcloud init` start configuration of gcloud
+* `gcloud app deploy --help` for detailed info on deployment and commands
+* `gcloud app deploy` to deploy application to the Cloud Platform
+* `gcloud app browse` to open the application via the browser on the command line
+* Docs on deployment through python (https://cloud.google.com/appengine/docs/flexible/python/testing-and-deploying-your-app)
+
+## Release History
+* 0.1.0 - Initial release
+
+## License
+
+MIT © [Ricardo Raphael Joson](http://rrjoson.com)
---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# General
+from webapp2 import WSGIApplication
+from google.appengine.ext import db
+from helpers import *
+
+
+# Models
+from models.user import User
+from models.post import Post
+from models.like import Like
+from models.comment import Comment
+
+
+# Handlers
+from handlers.blog import BlogHandler
+from handlers.blogfront import BlogFrontHandler
+from handlers.signup import SignupHandler
+from handlers.login import LoginHandler
+from handlers.logout import LogoutHandler
+from handlers.post import PostHandler
+from handlers.newpost import NewPostHandler
+from handlers.editpost import EditPostHandler
+from handlers.deletepost import DeletePostHandler
+from handlers.likepost import LikePostHandler
+from handlers.unlikepost import UnlikePostHandler
+from handlers.addcomment import AddCommentHandler
+from handlers.editcomment import EditCommentHandler
+from handlers.deletecomment import DeleteCommentHandler
+
+
+# Routing
+app = WSGIApplication([
+    ('/', BlogFrontHandler),
+    ('/signup', SignupHandler),
+    ('/login', LoginHandler),
+    ('/logout', LogoutHandler),
+    ('/newpost', NewPostHandler),
+    ('/([0-9]+)', PostHandler),
+    ('/([0-9]+)/like', LikePostHandler),
+    ('/([0-9]+)/unlike', UnlikePostHandler),
+    ('/([0-9]+)/edit', EditPostHandler),
+    ('/([0-9]+)/delete/([0-9]+)', DeletePostHandler),
+    ('/([0-9]+)/addcomment/([0-9]+)', AddCommentHandler),
+    ('/([0-9]+)/([0-9]+)/editcomment/([0-9]+)', EditCommentHandler),
+    ('/([0-9]+)/([0-9]+)/deletecomment/([0-9]+)', DeleteCommentHandler)
+], debug=True)
